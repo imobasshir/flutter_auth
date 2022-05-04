b088bb8fbebcbaf93a009a805505bf12388946e2
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-05-04 21:48:20 +0530

    flutter configuration with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_auth/firebase_options.dart';
 import 'package:flutter_auth/screens/login_screen.dart';
 import 'package:flutter_auth/screens/loginpage_screen.dart';
 import 'package:flutter_auth/screens/phone_screen.dart';
 import 'package:flutter_auth/screens/signup_screen.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
